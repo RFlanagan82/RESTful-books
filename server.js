0e9d16759aa0a94f20cdf17ddd11c520e4fc3b9e
Ryan Flanagan <flanagan.ryan@gmail.com>
2021-01-11 15:27:14 -0500

    set up Resource Driven API routes on Server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require ('express');
+const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
@@ -15,29 +15,67 @@ mongoose.connect(
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
-  });
+  }
+);
+
+// set up connection to constructed Database
+const db = require("./models");
 
 //Check to see if Mongoose connection is running
 const connection = mongoose.connection;
 
 //  If connected to Mongoose
-connection.on("connected", ()=> {
-    console.log("Mongoose successfully connected.");
+connection.on("connected", () => {
+  console.log("Mongoose successfully connected.");
 });
 
 // If an error connecting to Mongoose
 connection.on("error", (err) => {
-    console.log("Mongoose connection error: ", err);
-})
+  console.log("Mongoose connection error: ", err);
+});
 
-//Check to see if PORT is working
+//Check to see if PORT is working - use browser tab or Postman
 app.get("/api/config", (req, res) => {
-    res.json({
-        success: true,
-    });
+  res.json({
+    success: true,
+  });
 });
 
+// ====================================================================================
+//Setup our routes for Resource Driven API
+
+app.get("/api/book", (req, res) => {
+  db.Book.find({}).then((foundBooks) => {
+    res.json(foundBooks);
+  });
+});
+
+app.get("/api/book/:id", (req, res) => {
+  db.Book.find({ _id: req.params.id }).then((foundBook) => {
+    res.json(foundBook);
+  });
+});
+
+app.post("/api/book", (req, res) => {
+  db.Book.create(req.body).then((newBook) => {
+    res.json(newBook);
+  });
+});
+
+app.put("/api/book/:id", (req, res) => {
+  db.Book.findByIdAndUpdate(req.params.id, req.body, {new: true}).then((updatedBook) => {
+    res.json(updatedBook);
+  });
+});
+
+app.delete("/api/book/:id", (req, res) => {
+    db.Book.findByIdAndDelete(req.params.id).then((result) => {
+        res.json(result);
+    })
+});
+// ===================================================================================
+
+// Run App on the Port in the browser
 app.listen(PORT, () => {
-    console.log(`App is running on http://localhost${PORT}`);
+  console.log(`App is running on http://localhost${PORT}`);
 });
-
