5cb2e6c10495ab7a63f85d587c87b3800f260842
Ryan Flanagan <flanagan.ryan@gmail.com>
2021-01-15 14:59:42 -0500

    set up views pages in containers directory and initialized App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,12 @@
 import { useEffect } from "react";
 import axios from "axios";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import NewBook from "./containers/NewBook/NewBook";
+import EditBook from "./containers/EditBook/EditBook";
+import SingleBook from "./containers/SingleBook/SingleBook";
+import AllBooks from "./containers/AllBooks/AllBooks";
+import NotFound from "./containers/NotFound/NotFound";
+import Home from "./containers/Home/Home";
 
 function App() {
   useEffect(() => {
@@ -15,8 +22,20 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
+    <div>
+      {/* Installed React-Router-Dom to use Router package */}
+      <Router>
+        <Switch>
+          {/* Need to have created a book and placed in the database before editing it or searching for it. That's why it follows in this order */}
+          <Route exact path="/books/new" component={NewBook} />
+          <Route exact path="/books/:bookId/edit" component={EditBook} />
+          <Route exact path="/books/:bookId" component={SingleBook} />
+          <Route exact path="/books" component={AllBooks} />
+          <Route exact path="/" component={Home} />
+          {/* Catch all page for any url not utilized above - i.e a 404 page. */}
+          <Route component={NotFound} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ client/src/containers/NotFound/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1>This item is not found. Are you lost?</h1>
+        </div>
+    );
+};
+
+export default NotFound;
