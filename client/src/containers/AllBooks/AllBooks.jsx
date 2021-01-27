4b9f064c6220f396e1efe72d7d392fa6402e64f9
Ryan Flanagan <flanagan.ryan@gmail.com>
2021-01-27 14:32:41 -0500

    new book form working but need to add author to it

--- client/src/containers/AllBooks/AllBooks.jsx
+++ client/src/containers/AllBooks/AllBooks.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 const AllBooks = () => {
   const [books, setBooks] = useState([]);
@@ -13,6 +14,13 @@ const AllBooks = () => {
   return (
     <div className="container">
       <div className="row">
+        <div className="col-sm-12 text-right">
+          <Link to="/books/new" className="btn btn-primary">
+            New Book
+          </Link>
+        </div>
+      </div>
+      <div className="row">
         <div className="col-sm-12">
           <table className="table table-striped">
             <thead className="table-dark">
@@ -20,6 +28,8 @@ const AllBooks = () => {
                 <th scope="col">Title</th>
                 <th scope="col">Pages</th>
                 <th scope="col">Author</th>
+                <th></th>
+                <th></th>
               </tr>
             </thead>
             <tbody>
@@ -29,6 +39,12 @@ const AllBooks = () => {
                     <td>{book.title}</td>
                     <td>{book.pages}</td>
                     <td>{book.author && book.author.fullName}</td>
+                    <td>
+                      <button className="btn btn-secondary">Edit</button>
+                    </td>
+                    <td>
+                      <button className="btn btn-danger">Delete</button>
+                    </td>
                   </tr>
                 ))
               ) : (
--- client/src/containers/NewBook/NewBook.jsx
+++ client/src/containers/NewBook/NewBook.jsx
@@ -1,10 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from "axios";
 
 const NewBook = () => {
+    const [title, setTitle] = useState("");
+    const [pages, setPages] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios
+        .post("/api/books", {title, pages})
+        .then((response) => {
+            console.log(response.data);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    }
+
     return (
-        <div>
-            <h1>A new book form will go here.</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-3"></div>
+          <div className="col-sm-6">
+            <form onSubmit={handleSubmit}>
+              <div className="form-group">
+                <label htmlFor="title" className="form-label">
+                  Title
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="title"
+                  name="title"
+                  value={title}
+                  onChange={(e) => {
+                      setTitle(e.target.value)
+                  }}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="pages" className="form-label">
+                  Pages
+                </label>
+                <input type="text" className="form-control" 
+                id="pages"
+                name={pages}
+                value={pages}
+                onChange={(e) =>{
+                    setPages(e.target.value);
+                }} 
+                />
+              </div>
+              <div className="text-center">
+                <button type="submit" className="btn btn-primary">
+                  Create New Book
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
+      </div>
     );
 };
 
